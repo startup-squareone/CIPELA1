77fb8ff27cc32966d0ae68566f55e35280c94804
startup-squareone <144127675+startup-squareone@users.noreply.github.com>
2023-09-05 18:28:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,63 @@
-// script.js
+// Define product data
+const product = {
+    title: "Product Title",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    price: 99.99,
+    image: "product-image.jpg"
+};
 
-document.getElementById('customizeButton').addEventListener('click', function() {
-    document.getElementById('customizationTool').classList.toggle('hidden');
-    document.getElementById('footMeasurementTool').classList.add('hidden');
+// Define customization options (you can expand this)
+const customizationOptions = [
+    "Color",
+    "Size",
+    "Material",
+    // Add more options here
+];
+
+// Event listener for the Customize button
+document.getElementById("customizeButton").addEventListener("click", () => {
+    // Display customization options and tools
+    displayCustomizationOptions();
 });
 
-document.getElementById('measureButton').addEventListener('click', function() {
-    document.getElementById('footMeasurementTool').classList.toggle('hidden');
-    document.getElementById('customizationTool').classList.add('hidden');
+// Event listener for the Measure Your Foot button
+document.getElementById("measureButton").addEventListener("click", () => {
+    // Display foot measurement tools
+    displayFootMeasurementTools();
 });
+
+// Function to display customization options
+function displayCustomizationOptions() {
+    const customizationTool = document.getElementById("customizationTool");
+    const customizationForm = document.createElement("form");
+
+    // Add options to the form
+    customizationOptions.forEach((option) => {
+        const label = document.createElement("label");
+        label.textContent = option;
+
+        const input = document.createElement("input");
+        input.type = "text"; // You can use select elements for options
+
+        customizationForm.appendChild(label);
+        customizationForm.appendChild(input);
+    });
+
+    customizationTool.innerHTML = ""; // Clear previous content
+    customizationTool.appendChild(customizationForm);
+    customizationTool.style.display = "block";
+}
+
+// Function to display foot measurement tools
+function displayFootMeasurementTools() {
+    const footMeasurementTool = document.getElementById("footMeasurementTool");
+
+    // Create and display measurement tools (you can expand this)
+    const measurementToolHTML = `
+        <h3>Foot Measurement Tools</h3>
+        <!-- Add your measurement tools here -->
+    `;
+
+    footMeasurementTool.innerHTML = measurementToolHTML;
+    footMeasurementTool.style.display = "block";
+}
