0c99ab63be61c8eedb3dafe25dc1eb4a1eade5d5
startup-squareone <144127675+startup-squareone@users.noreply.github.com>
2023-09-07 18:59:18 +0530

    Create home.js

---
+++ home.js
@@ -0,0 +1,18 @@
+// script.js
+
+const slides = document.querySelectorAll('.slide');
+let currentSlide = 0;
+
+function showSlide(index) {
+    slides[currentSlide].style.opacity = 0;
+    currentSlide = (index + slides.length) % slides.length;
+    slides[currentSlide].style.opacity = 1;
+}
+
+// Auto-advance the slider every 3 seconds
+setInterval(() => {
+    showSlide(currentSlide + 1);
+}, 3000);
+
+// Initial slide display
+showSlide(currentSlide);
