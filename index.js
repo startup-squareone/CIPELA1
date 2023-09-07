24a3329034e71570859605e9c4f6452b0e5950bc
startup-squareone <144127675+startup-squareone@users.noreply.github.com>
2023-09-07 18:56:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,25 +14,6 @@ const customizationOptions = [
     // Add more options here
 ];
 
-// script.js
-
-const slides = document.querySelectorAll('.slide');
-let currentSlide = 0;
-
-function showSlide(index) {
-    slides[currentSlide].style.opacity = 0;
-    currentSlide = (index + slides.length) % slides.length;
-    slides[currentSlide].style.opacity = 1;
-}
-
-// Auto-advance the slider every 3 seconds
-setInterval(() => {
-    showSlide(currentSlide + 1);
-}, 3000);
-
-// Initial slide display
-showSlide(currentSlide);
-
 
 // Event listener for the Customize button
 document.getElementById("customizeButton").addEventListener("click", () => {
